1c82e272d40036979b7943dfc25ce97121cf5f44
Nandagopal <nandagopal.yemineni@gmail.com>
2012-11-28 05:19:12 +0530

    Update src/com/one/two/Test1.java

--- src/com/one/two/Test1.java
+++ src/com/one/two/Test1.java
@@ -8,7 +8,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		 System.out.println("in test1");
 		 System.out.println("in test1");
-		 System.out.println("in test1");
+		 
 
 	}
 
