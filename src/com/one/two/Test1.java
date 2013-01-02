3ca7c201e80b87429ff1cbb35f418738644dd293
NamdaGopal Yemineni <nandagopal.yemineni@gmail.com>
2013-01-03 00:41:14 +0530

    committing to the repo

--- src/com/one/two/Test1.java
+++ src/com/one/two/Test1.java
@@ -7,13 +7,9 @@ public class Test1 {
 	 */
 	public static void main(String[] args) {
 		 System.out.println("in test1");
-<<<<<<< HEAD
-		 System.out.println("in test1");
-		 System.out.println("in test1");
-=======
 		 System.out.println("in test2");
-		 
->>>>>>> origin/master
+		 System.out.println("in test3");
+
  	}
 
 }
