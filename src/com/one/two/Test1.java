3deca4d5ade771ee0e6e711851f7225f410c8b8a
Nanda Gopal <nandagopal.y@myiton.com>
2013-01-28 12:09:23 +0530

    testing for reset and checkout

--- src/com/one/two/Test1.java
+++ src/com/one/two/Test1.java
@@ -19,7 +19,7 @@ public class Test1 {
 		 System.out.println("3333333333333");
 		 System.out.println("4444444444444");
 		 System.out.println("2222222222222");
-
+		 System.out.println("6666666666666");
  	}
 
 }
